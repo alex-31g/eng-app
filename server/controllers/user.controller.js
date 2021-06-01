fe37aba040368c9d0ac8ae0efd21b1c3df372720
alex-31g <al.tsimbalyuk@gmail.com>
2021-06-01 17:07:10 +0300

    Fix DB_PASSWORD in .env file

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,14 @@ const ApiError = require("../error/api.error");
 class UserController {
   async checkRegistration(req, res) {}
 
+  // test error
+  // async checkRegistration(req, res, next) {
+  //   const { id } = req.query;
+  //   if (!id) {
+  //     next(ApiError.badRequest("no id"));
+  //   }
+  // }
+
   async getUserData(req, res) {}
 
   async registration(req, res) {}
